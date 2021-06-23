b40afbeabc10dc85477ca9cd422e63f14589b213
cngneee <86335263+cngneee@users.noreply.github.com>
2021-06-23 09:45:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'hello bé iuuuu!,em an sang chuaa :<',
+    introTitle: 'hello bé iuuuu!,em an sang chuaa :< ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'bấm zooo đây nèeeeee!',
+    btnIntro: 'bấm zooo đây nèeeeee! ',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'em iuu anhhhh <3',
-    btnNo: 'Không, chỉ xem là anh trai hoyy àaa :'((((',
+    btnYes: 'em iuu anhhhh <3 ',
+    btnNo: 'Không, chỉ xem là anh trai hoyy àaa :'(((( ',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'vi anh dep trai <3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'chúc em buổi sáng tốt lành, chìu nay thi thiệc tốt nheaaa công chúa.',
-    btnAccept: 'nho em nhieu <3',
+    btnAccept: 'nho em nhieu <3 ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
